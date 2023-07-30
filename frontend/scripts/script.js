04ac662ac4dd452002b346aa1dffd5e96386832c
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-07-30 19:04:41 +0300

    <FE> add "add to favorites" listener

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,7 +1,31 @@
 const account_button = document.getElementById("account-button");
 const account_tab = document.getElementById("account-tab");
 
+let favorites = [];
+
+
+const product_states = ["nofav_nocart","fav_nocart","nofav_cart","fav_cart"]
+
+localStorage.setItem("favorites", JSON.stringify(favorites));
+
 
 account_button.addEventListener("click", () => {
-    account_tab.classList.toggle("show")
+	account_tab.classList.toggle("show");
+});
+
+const add_to_cart = document.getElementById("add-to-cart");
+
+const add_to_favorites = document.getElementById("add-to-favorites");
+
+add_to_favorites.addEventListener("mouseover", () => {
+	add_to_favorites.src = "assets/icons/filled/favorite.svg";
+});
+
+add_to_favorites.addEventListener("mouseout", () => {
+	add_to_favorites.src = "assets/icons/black/heart_plus.svg";
+});
+
+add_to_favorites.addEventListener("click", (e) => {
+    favorites.push(e.target.parentElement.id)
+    localStorage.setItem("favorites", JSON.stringify(favorites));
 });
