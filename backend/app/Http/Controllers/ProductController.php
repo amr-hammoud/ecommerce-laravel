99050d380ca3def98d3e062a770eb4447df64fb0
Amr Hammoud <amrhammoud.dev@gmail.com>
2023-08-01 08:43:05 +0300

    <BE> enable jwt middleware

--- backend/app/Http/Controllers/ProductController.php
+++ backend/app/Http/Controllers/ProductController.php
@@ -7,10 +7,10 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    // public function __construct()
-    // {
-    //     $this->middleware('auth:api');
-    // }
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
 
     public function get($id = null){
 
